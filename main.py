8ce72be3c0924cbf42310f10aef0f26a0da92621
Austin Chayka <austinchayka@gmail.com>
2018-08-26 14:59:28 -0700

    Uploaded code
    Uploaded the initial project code to github

---
+++ main.py
@@ -0,0 +1,32 @@
+from neuralNet import NeuralNetwork
+import numpy as np
+
+#create a network with 3 inputs, 3 hidden layers (the first with 5 nodes
+#the second with 5 nodes, and the third with 3 nodes), and a single output 
+neuralNet = NeuralNetwork(3, [5, 5, 3], 1)
+
+#create input data set
+ni = np.array([[1, 0, 1],
+               [1, 0, 0],
+               [1, 1, 1],
+               [0, 0, 0],
+               [1, 1, 0],
+               [0, 1, 0],
+               [0, 0, 1],
+               [0, 1, 1]])
+
+#create output data set
+no = np.array([[0],
+               [1],
+               [0],
+               [0],
+               [0],
+               [1],
+               [1],
+               [0]])
+
+#train network with data sets
+neuralNet.train(ni, no, 100000)
+
+#test an input set for accuracy
+print(neuralNet.think(np.array([0, 1, 1])))
---
+++ neuralNet.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+#seed the neural net for consistency in testing
+np.random.seed(1)
+
+
+class NeuralNetwork(object):
+
+
+  #constructor sets up hidden layers and network nodes
+  def __init__(self, inputs, hidden_layers, outputs):
+    self.layers = [None] * (len(hidden_layers) + 1)
+
+    for i in range(len(self.layers)):
+      if(i == 0):
+         self.layers[0] = 2 * np.random.random((inputs, hidden_layers[i])) - 1
+      elif(i == len(hidden_layers)):
+         self.layers[i] = 2 * np.random.random((hidden_layers[i - 1], outputs)) - 1
+      else:
+         self.layers[i] = 2 * np.random.random((hidden_layers[i - 1], hidden_layers[i])) - 1
+
+    for layer in self.layers:
+      print(layer, "\n")
+
+
+  #node activation function
+  def sigmoid(self, x):
+      return 1 / (1 + np.exp(-x))
+
+
+  #derivative of activation function used for back-propogation
+  def sigmoid_derivative(self, x):
+    return x * (1 - x)
+
+
+  #forward-propogation method
+  def think(self, x):
+    o2 = self.sigmoid(np.dot(x, self.layers[0]))
+    for i in range(len(self.layers) - 1):
+      o = self.sigmoid(np.dot(o2, self.layers[i + 1]))
+      o2 = o
+    return o
+
+
+  #training method using back-propogation
+  def train(self, xi, xf, iterations):
+    
+    for iteration in range(iterations):
+      
+      #create an array to hold outputs and errors for each layer
+      outs = [None] * (len(self.layers))
+      for i in range(len(outs)):
+        outs[i] = [None] * 3
+
+      #forward-propogation of input data
+      for i in range(len(self.layers)):
+        if(i == 0):
+          outs[0][0] = self.sigmoid(np.dot(xi, self.layers[i]))
+        else:
+          outs[i][0] = self.sigmoid(np.dot(outs[i - 1][0], self.layers[i]))
+
+      #calculate and store the error and error delta for each layer
+      for i in range(len(self.layers)):
+        if(i == 0):
+          outs[len(outs) - 1][1] = xf - outs[len(outs) - 1][0]
+          outs[len(outs) - 1][2] = outs[len(outs) - 1][1] * self.sigmoid_derivative(outs[len(outs) - 1][0])
+        else:
+          outs[len(outs) - 1 - i][1] = outs[len(outs) - i][2].dot(self.layers[len(self.layers) - i].T)
+          outs[len(outs) - 1 - i][2] = outs[len(outs) - 1 - i][1] * self.sigmoid_derivative(outs[len(outs) - 1 - i][0])
+
+      #print out square of errors
+      if(iteration % 1000 == 0):
+        square_errors = 0
+        for array in outs:
+          square_errors += np.sum(array[1] * array[1])
+        print(square_errors)
+
+      #adjust each layer acording to the error delta
+      for i in range(len(outs)):
+        if(i == 0):
+          self.layers[0] += xi.T.dot(outs[0][2])
+        else:
+          self.layers[i] += outs[i - 1][0].T.dot(outs[i][2])
